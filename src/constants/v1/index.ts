e02d384f4fb0bf227d59bb9936b2589d1fffe9de
Hanoverchee <84304844+Hanoverchee@users.noreply.github.com>
2021-05-21 19:28:59 +0700

    Update index.ts

--- src/constants/v1/index.ts
+++ src/constants/v1/index.ts
@@ -4,8 +4,8 @@ import V1_EXCHANGE_ABI from './v1_exchange.json'
 import V1_FACTORY_ABI from './v1_factory.json'
 
 const V1_FACTORY_ADDRESSES: { [chainId in ChainId]: string } = {
-  [ChainId.MAINNET]: '0xF7Cc04B435d590e20bf7f0B67C5aA6B43177aB0A', //TODO
-  [ChainId.BSCTESTNET]: '0xF7Cc04B435d590e20bf7f0B67C5aA6B43177aB0A'
+  [ChainId.MAINNET]: '0xd954551853F55deb4Ae31407c423e67B1621424A', //TODO
+  [ChainId.BSCTESTNET]: '0xd954551853F55deb4Ae31407c423e67B1621424A'
 }
 
 const V1_FACTORY_INTERFACE = new Interface(V1_FACTORY_ABI)
